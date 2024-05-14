e3b2425a6fd443429e0d353eeddbd46400757efd
Aidan James <ajame23@uwo.ca>
2024-05-14 12:53:28 -0400

    Initial Commit

---
+++ golf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  golf
+//
+//  Created by Aidan James on 2024-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ golf/golfApp.swift
@@ -0,0 +1,17 @@
+//
+//  golfApp.swift
+//  golf
+//
+//  Created by Aidan James on 2024-05-14.
+//
+
+import SwiftUI
+
+@main
+struct golfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
